3a4255ddb3f20028058fada313f1a970df01f49a
Juan Antonio Gaitan Alvarez <juangaitan.gt@outlook.com>
2025-11-13 01:00:22 -0600

    Creación de Clases
    Se realizo la creación de todas las clases base del proyecto

--- src/UTIL/HashPasword.java
+++ src/UTIL/HashPasword.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package MODELO;
+package UTIL;
 
 /**
  *
  * @author juangaitan
  */
-public class libro {
+public class HashPasword {
     
 }
